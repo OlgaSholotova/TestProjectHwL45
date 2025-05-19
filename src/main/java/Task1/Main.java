6691dd20c241a350cbb99586ff6c46830575c30f
OlgaSholotova <olga.burkut@gmail.com>
2025-05-19 22:54:21 +0200

    Task1: внесены изменения для второго коммита первого PR

--- src/main/java/Task1/Main.java
+++ src/main/java/Task1/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Student student = new Student();
         System.out.println(student);
+        System.out.println("Изменения для второго коммита первого PR");
     }
 }
--- src/main/java/Task1/Student.java
+++ src/main/java/Task1/Student.java
@@ -3,4 +3,5 @@ package Task1;
 public class Student {
    String name;
    int age;
+   int ball;
 }
